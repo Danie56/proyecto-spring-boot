3a7977bb50b1072bc58f440d2fdc0c18102087e5
Danie56 <danisanto0179@gmail.com>
2025-10-26 22:29:33 -0500

    implement ProductApi interface

--- src/main/java/com/example/proyecto_spring_boot/product/infrastructure/ProductApi.java
+++ src/main/java/com/example/proyecto_spring_boot/product/infrastructure/ProductApi.java
@@ -1,4 +1,21 @@
 package com.example.proyecto_spring_boot.product.infrastructure;
 
+import com.example.proyecto_spring_boot.product.domain.Product;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 public interface ProductApi {
+
+    ResponseEntity<List<Product>> getAllProducts();
+
+    ResponseEntity<Product> getProductById(@PathVariable Long id);
+
+    ResponseEntity<Void> createProduct(@RequestBody Product product);
+
+    ResponseEntity<Product> updateProduct(@RequestBody Product product);
+
+    ResponseEntity<Void> deleteProduct(@PathVariable Long id);
 }
