8e27e37f4f995fded7a361f4190dc3106232c68f
Danie56 <danisanto0179@gmail.com>
2025-10-26 22:24:51 -0500

    crud add put
    # Conflicts:
    #	src/main/java/com/example/proyecto_spring_boot/product/infrastructure/ProductController.java

--- src/main/java/com/example/proyecto_spring_boot/product/infrastructure/ProductController.java
+++ src/main/java/com/example/proyecto_spring_boot/product/infrastructure/ProductController.java
@@ -25,6 +25,16 @@ public class ProductController {
 
         return ResponseEntity.ok(products);
 
+        this.products = new ArrayList<>();
+        products.add(Product.builder().id(1L).name("Product 1").description("Description 1").price(111.0).image("imagen 1").build());
+        products.add(Product.builder().id(2L).name("Product 2").description("Description 2").price(111.0).image("imagen 2").build());
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Product>> getAllProducts() {
+
+        return ResponseEntity.ok(products);
+
     }
 
     @GetMapping("/{id}")
@@ -40,6 +50,41 @@ public class ProductController {
         products.add(product);
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Optional<Product> productOptional = products.stream().filter(p -> p.getId().equals(id)).findFirst();
+        return productOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
+
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Void> createProduct(@RequestBody Product product) {
+        products.add(product);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("")
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product) {
+        Product productSelected = products.stream().filter(p -> p.getId().equals(product.getId())).findFirst().orElseThrow(() -> new RuntimeException("porduct not found"));
+        productSelected.setName(product.getName());
+        productSelected.setDescription(product.getDescription());
+        productSelected.setPrice(product.getPrice());
+        productSelected.setImage(product.getImage());
+        return ResponseEntity.ok(product);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        products.removeIf(product -> product.getId().equals(id));
+        return ResponseEntity.noContent().build();
+
+
+    }
+
+
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
